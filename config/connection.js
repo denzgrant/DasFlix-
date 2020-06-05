e3f7ae7cb652592517bb05b5bd2d556e8f77aba1
Stephen Studer <stephenkstuder@gmail.com>
2020-06-04 21:31:10 -0700

    jaws db stuff added

---
+++ config/connection.js
@@ -0,0 +1,28 @@
+const mysql = require("mysql");
+const util = require("util");
+
+let connection;
+
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "burgers_db",
+  });
+};
+
+connection.connect((error) => {
+  if (error) {
+    throw error;
+  }
+
+  console.log(`Connected to database as id ${connection.threadId}`);
+});
+
+connection.query = util.promisify(connection.query);
+
+module.exports = connection;
