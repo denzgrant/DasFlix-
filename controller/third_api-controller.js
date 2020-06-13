f9982d40cfff31d755861d5f5133f953df3d7760
Denzell Grant <denzgrant@gmail.com>
2020-06-13 15:00:49 -0700

    updated api

--- controller/third_api-controller.js
+++ controller/third_api-controller.js
@@ -17,26 +17,18 @@ router.get("/api/mediaSearch/:searchTerm", async (req, res) => {
         country: "us",
       },
     });
-    const secondData = await axios.get("https://movie-database-imdb-alternative.p.rapidapi.com/", {
-      headers: {
-        "content-type": "application/octet-stream",
-        "x-rapidapi-host": "movie-database-imdb-alternative.p.rapidapi.com",
-        "x-rapidapi-key": "ff26cadbd3msh6f81da38fd7ebd2p184197jsne85e8fe7ff15",
-        useQueryString: true,
-      },
-      params: {
-        i: `${firstData.data.results[0].external_ids.imdb.id}`,
-        r: "json",
-      },
+    const secondData = await axios.get(`https://api.themoviedb.org/3/search/multi?api_key=68c0e41b28df67801658d2f261ee4403&language=en-US&query=${req.params.searchTerm}&page=1&include_adult=false`, {
+
     });
     res.json({
       mediaTitle: firstData.data.results[0], //Title of show/movie
       mediaLocations: firstData.data.results[0].locations, //where to find show/movie
       //type as in TV or Movie
-      mediaType: secondData.data.Type,
-      mediaPlot: secondData.data.Plot,
-      mediaYear: secondData.data.Year,
-      mediaPoster: secondData.data.Poster,
+      mediaType: secondData.data.results[0].media_type,
+      mediaPlot: secondData.data.results[0].overview,
+      movieYear: secondData.data.results[0].release_date,
+      seriesYear: secondData.data.results[0].first_air_date,
+      mediaPoster: secondData.data.results[0].poster_path,
     });
   } catch (error) {
     res.status(500).send(error);
