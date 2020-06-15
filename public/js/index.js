4b9d7fde47688d556dd930c75b82e1d2ee15ae0e
Stephen Studer <stephenkstuder@gmail.com>
2020-06-15 06:55:09 -0700

    carosel working without the flip cards, will need to add that back in, but its a start at least...

--- public/js/index.js
+++ public/js/index.js
@@ -1,56 +1,81 @@
-var $submitBtn = $("#submit");
-// var $exampleList = $("#example-list");
-
-// let exampleMovies = [{ title: "James Bond" }, { title: "James Bond" }, { title: "James Bond" }, { title: "James Bond" }, { title: "James Bond" }];
-
-// // The API object contains methods for each kind of request we'll make
-// var API = {
-//   saveExample: function (example) {
-//     return $.ajax({
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       type: "POST",
-//       url: "api/examples",
-//       data: JSON.stringify(example),
-//     });
-//   },
-//   getExamples: function () {
-//     return $.ajax({
-//       url: "api/examples",
-//       type: "GET",
-//     });
-//   },
-//   deleteExample: function (id) {
-//     return $.ajax({
-//       url: "api/examples/" + id,
-//       type: "DELETE",
-//     });
-//   },
-// };
-
-//$("#submit").on("click", function () {
-//   event.preventDefault();
-//   const searchedMedia = $("#submit-query").val().trim().toLowerCase();
-
-//   $.get(`/api/media-search/${searchedmedia}`, function (data) {
-//     console.log(data);
-//     if (data) {
-//       $(".flip-card-front").append(`
-//         <img src="${mediaPoster}" alt="poster of movie/tv">`);
-//       $(".flip-card-back").append(`
-//           <h1>${mediaTitle}</h1>
-//           <p>Type ${mediaType}\n
-//           Year Released: ${mediaYear}\n
-//           Plot: ${mediaPlot}</p>
-//           <strong>Available on ${mediaLocations}</strong>`);
-//     } else {
-//       //prompt an error message
-//     }
-//   });
-// });
-
 $(document).ready(() => {
+
+  function wait(){
+    let slider = document.querySelector(".glide");
+    
+      let glide = new Glide(slider, {
+        type: "carousel",
+        autoplay: 3500,
+        perView: 6,
+      });
+    
+      glide.mount();
+  }
+
+  setTimeout(wait, 1000);
+
+  // new Glide('#glide1', {
+  // 	type: 'carousel',
+  // 	perView: 3,
+  // 	breakpoints: {
+  // 		800: {
+  // 			perView: 1
+  // 		}
+  // 	}
+  // }).mount();
+  //let $submitBtn = $("#submit");
+
+  // var $exampleList = $("#example-list");
+
+  // let exampleMovies = [{ title: "James Bond" }, { title: "James Bond" }, { title: "James Bond" }, { title: "James Bond" }, { title: "James Bond" }];
+
+  // // The API object contains methods for each kind of request we'll make
+  // var API = {
+  //   saveExample: function (example) {
+  //     return $.ajax({
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //       type: "POST",
+  //       url: "api/examples",
+  //       data: JSON.stringify(example),
+  //     });
+  //   },
+  //   getExamples: function () {
+  //     return $.ajax({
+  //       url: "api/examples",
+  //       type: "GET",
+  //     });
+  //   },
+  //   deleteExample: function (id) {
+  //     return $.ajax({
+  //       url: "api/examples/" + id,
+  //       type: "DELETE",
+  //     });
+  //   },
+  // };
+
+  //$("#submit").on("click", function () {
+  //   event.preventDefault();
+  //   const searchedMedia = $("#submit-query").val().trim().toLowerCase();
+
+  //   $.get(`/api/media-search/${searchedmedia}`, function (data) {
+  //     console.log(data);
+  //     if (data) {
+  //       $(".flip-card-front").append(`
+  //         <img src="${mediaPoster}" alt="poster of movie/tv">`);
+  //       $(".flip-card-back").append(`
+  //           <h1>${mediaTitle}</h1>
+  //           <p>Type ${mediaType}\n
+  //           Year Released: ${mediaYear}\n
+  //           Plot: ${mediaPlot}</p>
+  //           <strong>Available on ${mediaLocations}</strong>`);
+  //     } else {
+  //       //prompt an error message
+  //     }
+  //   });
+  // });
+
   /////////////////////////////////////////////////////////////////////////////////////////
   //3 party API call
   /////////////////////////////////////////////////////////////////////////////////////////
@@ -80,8 +105,17 @@ $(document).ready(() => {
   /////////////////////////////////////////////////////////////////////////////////////////
   let queryTrending = () => {
     let queryURL = `http://localhost:8080/api/trending/`;
-
     let data;
+    let glideContainer = `      
+    <div class="glide">
+    <div class="glide__track" data-glide-el="track">
+      <ul class="glide__slides" id="movie-bottom-list">        
+      </ul>
+    </div>
+  </div>
+`;
+$("#search").append(glideContainer);
+
     $.ajax({
       url: queryURL,
       method: "GET",
@@ -92,24 +126,16 @@ $(document).ready(() => {
         let movieArray = response.media;
         movieArray.forEach((movie) => {
           let thisMovieCard = `
-<li>
-<div class="flip-card">
-<div class="flip-card-inner">
-  <div class="flip-card-front">
-      <img src="http://image.tmdb.org/t/p/w185//${movie.poster_path}" alt="${movie.title}">
-  </div>
+<li class="glide__slide">
+<img src="http://image.tmdb.org/t/p/w185//${movie.poster_path}" alt="${movie.title}">
   <div class="flip-card-back">
       <h2> ${movie.title} </h2>
       <p> ${movie.overview} </p>
       <button></button>
   </div>
-</div>
-</div>
-<div class="uk-position-center uk-panel">
-</div>
 </li>
 `;
-          $("#bottom-movie-list").append(thisMovieCard);
+          $("#movie-bottom-list").append(thisMovieCard);
         });
         //$("#theData").text(response.mediaTitle);
       });
@@ -167,7 +193,7 @@ $(document).ready(() => {
   /***************************************************************************************/
   //-	Create a movie (to put inside of list)
   /***************************************************************************************/
-});
 
-//$submitBtn.on("click", handleFormSubmit);
-//$exampleList.on("click", ".delete", handleDeleteBtnClick)
+  //$submitBtn.on("click", handleFormSubmit);
+  //$exampleList.on("click", ".delete", handleDeleteBtnClick)
+});
